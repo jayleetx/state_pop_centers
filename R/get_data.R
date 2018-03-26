4628246148771e3f356fd36fee5e07b6742ce260
jayleetx <jaylee@reed.edu>
2018-03-26 11:49:30 -0700

    Changed data source

--- R/get_data.R
+++ R/get_data.R
@@ -1,14 +1,18 @@
 library(rvest)
 library(raster)
+library(jsonlite)
+library(dplyr)
 
 # state population centers since 1880 census
 download.file("http://www2.census.gov/geo/docs/reference/cenpop2010/nat_cop_1880_2010.txt", 'data/state_pop_centers.csv')
 
-state_capital_url <- 'https://www.jetpunk.com/data/countries/united-states/state-capitals-list'
-xml <- read_html(state_capital_url)
-node <- html_node(x = xml, xpath = '//*[@id="data-table"]')
-q <- html_table(node)
-write.table(q, file = 'data/state_capitals.csv', quote = FALSE, sep = ',', row.names = FALSE)
+state_capital_url <- 'https://gist.githubusercontent.com/jpriebe/d62a45e29f24e843c974/raw/b1d3066d245e742018bce56e41788ac7afa60e29/us_state_capitals.json'
+caps <- fromJSON(state_capital_url)
+state_capitals <- data.frame(do.call(rbind, caps), stringsAsFactors = FALSE, row.names = NULL)
+state_capitals <- c("United States", "Washington, D.C.", "38.89511", "-77.03637") %>%
+  rbind(apply(state_capitals, 2, unlist)) %>%
+  data.frame(stringsAsFactors = FALSE)
+write.csv(state_capitals, file = "data/state_capitals.csv", row.names = FALSE)
 
 temp <- tempfile()
 download.file("http://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_state_20m.zip",temp)
