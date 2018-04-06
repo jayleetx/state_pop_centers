1ef50090b08fa44a5e7c86520183606c4e8b0e07
jayleetx <jaylee@reed.edu>
2018-04-06 11:58:21 -0700

    Add state postal code to capitals df

--- R/get_data.R
+++ R/get_data.R
@@ -2,9 +2,18 @@ library(rvest)
 library(raster)
 library(jsonlite)
 library(dplyr)
+library(tidyr)
 library(stringr)
 library(maps)
 data(us.cities)
+
+abbs <- c(state.abb, "US", "DC")
+names <- c(state.name, "United States", "District of Columbia")
+# this is a pretty bad function tbh but it works here
+swap_abb_name <- function(x) {
+  abbs[names == x]
+}
+  
 
 ##### state population centers since 1880 census #####
 download.file("http://www2.census.gov/geo/docs/reference/cenpop2010/nat_cop_1880_2010.txt", 'data/state_pop_centers.csv')
@@ -19,9 +28,12 @@ state_capitals <- data.frame(do.call(rbind, caps), stringsAsFactors = FALSE, row
   mutate(name = as.character(name),
          capital = as.character(capital),
          lat = as.numeric(lat),
-         long = as.numeric(long))
-state_capitals[26, 2] <- "Helena" #typo fom "Helana"
+         long = as.numeric(long),
+         abb = swap_abb_name(name)) %>%
+  unite(capital, 2,5, sep = ", ")
+state_capitals[26, 2] <- "Helena, MT" #typo fom "Helana"
 state_capitals[34, 3] <- state_capitals[34, 3] - 2
+state_capitals[51:52, 2] <- "Washington, D.C."
 write.csv(state_capitals, file = "data/state_capitals.csv", row.names = FALSE)
 
 ##### biggest cities #####
@@ -34,7 +46,8 @@ pop_cities <- data.frame(large, stringsAsFactors = FALSE, row.names = NULL) %>%
   group_by(state) %>%
   top_n(1, population) %>%
   rbind(.[1, ]) %>%
-  data.frame(stringsAsFactors = FALSE)
+  data.frame(stringsAsFactors = FALSE) %>%
+  mutate(population = format(population, big.mark=",", scientific=FALSE, trim = TRUE))
 pop_cities[52, 1] <- "United States"
 pop_cities[52, 2] <- "New York, NY"
 write.csv(pop_cities, file = "data/pop_cities.csv", row.names = FALSE)
