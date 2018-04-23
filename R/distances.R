5368c8c395ba6d68a197b016802e2e3345790983
jayleetx <jaylee@reed.edu>
2018-04-23 12:03:14 -0700

    Start working on calculating distances - all coords in one dataframe

---
+++ R/distances.R
@@ -0,0 +1,17 @@
+library(geosphere)
+library(dplyr)
+
+load("data/pop_centers.RData")
+pop <- pop_centers %>%
+  filter(Year == 2010) %>%
+  dplyr::select(4,2,3)
+colnames(pop) <- c("state", "pop_lat", "pop_long")
+caps <- read.csv("data/state_capitals.csv", stringsAsFactors = FALSE)[-2]
+colnames(caps) = c("state", "cap_lat", "cap_long")
+cities <- read.csv("data/pop_cities.csv", stringsAsFactors = FALSE)[ ,c(-2, -5)]
+colnames(cities) = c("state", "big_lat", "big_long")
+geo <- read.csv("data/geo_centers.csv", stringsAsFactors = FALSE)
+colnames(geo) <- c("state", "geo_lat", "geo_long")
+
+q <- list(pop, caps, cities, geo) %>%
+  Reduce(function(dtf1,dtf2) left_join(dtf1,dtf2,by="state"), .)
