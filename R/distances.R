092f0d8f75be486fe3ba8779167f144c7fc43e26
jayleetx <jaylee@reed.edu>
2018-04-25 10:21:27 -0700

    Add distance data

--- R/distances.R
+++ R/distances.R
@@ -13,5 +13,14 @@ colnames(cities) = c("state", "big_lat", "big_long")
 geo <- read.csv("data/geo_centers.csv", stringsAsFactors = FALSE)
 colnames(geo) <- c("state", "geo_lat", "geo_long")
 
-q <- list(pop, caps, cities, geo) %>%
-  Reduce(function(dtf1,dtf2) left_join(dtf1,dtf2,by="state"), .)
+distances <- list(pop, caps, cities, geo) %>%
+  Reduce(function(dtf1,dtf2) left_join(dtf1,dtf2,by="state"), .) %>%
+  transmute(state = state,
+            cap_dist_met = distCosine(.[ ,c('pop_long', 'pop_lat')], .[ ,c('cap_long', 'cap_lat')])/1000,
+            cap_dist_imp = conv_unit(cap_dist_met, "km", "mi"),
+            city_dist_met = distCosine(.[ ,c('pop_long', 'pop_lat')], .[ ,c('big_long', 'big_lat')])/1000,
+            city_dist_imp = conv_unit(city_dist_met, "km", "mi"),
+            geo_dist_met = distCosine(.[ ,c('pop_long', 'pop_lat')], .[ ,c('geo_long', 'geo_lat')])/1000,
+            geo_dist_imp = conv_unit(geo_dist_met, "km", "mi"))
+
+write.csv(distances, file = "data/distances.csv", row.names = FALSE)
